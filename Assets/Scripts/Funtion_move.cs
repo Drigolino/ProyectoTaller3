55f5250c7927bb92cb933fffbf14ff7eb63f3a9c
Drigolino <rodrigolino05@hotmail.com>
2021-08-18 10:36:27 -0400

    movement

--- Assets/Scripts/Funtion_move.cs
+++ Assets/Scripts/Funtion_move.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Funtion_move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    float _speed=1.0f;
+    [SerializeField]
+    GameObject Player;
+
+
     void Start()
     {
         
@@ -14,5 +19,37 @@ public class Funtion_move : MonoBehaviour
     void Update()
     {
         
+    }
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+     
+            transform.Translate(Vector3.right * _speed * 1.5f * horizontalInput * Time.deltaTime);
+            transform.Translate(Vector3.up * _speed * 1.5f * verticalInput * Time.deltaTime);
+
+        
+      
+        if (transform.position.y > 4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+        }
+        if (transform.position.y < -3.0f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.0f, 0);
+        }
+
+
+
+        if (transform.position.x >= 9)
+        {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        if (transform.position.x < -9f)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+
     }
 }
