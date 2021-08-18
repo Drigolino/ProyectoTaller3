29f0d3078f4f43b744765faa97fa5304ca39f2bd
Drigolino <rodrigolino05@hotmail.com>
2021-08-18 11:10:18 -0400

    movimiento personaje arreglado

--- Assets/Scripts/Funtion_move.cs
+++ Assets/Scripts/Funtion_move.cs
@@ -16,40 +16,5 @@ public class Funtion_move : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void Movement()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-     
-            transform.Translate(Vector3.right * _speed * 1.5f * horizontalInput * Time.deltaTime);
-            transform.Translate(Vector3.up * _speed * 1.5f * verticalInput * Time.deltaTime);
-
-        
-      
-        if (transform.position.y > 4.2f)
-        {
-            transform.position = new Vector3(transform.position.x, 4.2f, 0);
-        }
-        if (transform.position.y < -3.0f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.0f, 0);
-        }
-
-
-
-        if (transform.position.x >= 9)
-        {
-            transform.position = new Vector3(-9, transform.position.y, 0);
-        }
-        if (transform.position.x < -9f)
-        {
-            transform.position = new Vector3(9, transform.position.y, 0);
-        }
-
-    }
+  
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 1.0f;
+    [SerializeField]
+    GameObject _Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+     void Update()
+    {
+        Movement();
+        //if (Input.GetButtonDown(0))
+        //{
+        //    Vector3 mousePos = Input.mousePosition;
+        //    {
+        //        Debug.Log(mousePos.x);
+        //        Debug.Log(mousePos.y);
+        //    }
+        //}
+    }
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.right * _speed * 1.5f * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * 1.5f * verticalInput * Time.deltaTime);
+
+
+
+        if (transform.position.y > 4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+        }
+        if (transform.position.y < -3.0f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.0f, 0);
+        }
+
+
+
+        if (transform.position.x >= 9)
+        {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        if (transform.position.x < -9f)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+
+    }
+}
